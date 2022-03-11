29404533e40c75a8a52054a42b9cbd6751e4628b
richbruton <richbruton@gmail.com>
2022-03-11 16:26:46 +0000

    rough work notes for functions required

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+# build board
+
+# display board
+
+# store player + computer boats
+
+# verify player choice is on board(is an int and within 0, size -1) and not previously chosen
+# return hit or miss, store choices
+
+# round counter
+
+# game winner
+
