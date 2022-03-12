966ecec7b65c3fdb119d916a48478ea65546d047
richbruton <richbruton@gmail.com>
2022-03-12 10:05:42 +0000

    update show_board function to work for either player or computer

--- run.py
+++ run.py
@@ -8,6 +8,11 @@
 
 
 def board_size():
+    """
+    Function to allow the player to define board size
+    This will also eventually decide the number of boats
+    Validation to be add for max and min board size
+    """
     print("Board size selection\n")
     print("Board will be a square, so entering '5' will make a 5x5 board\n")
     print("Use a board size from 4 to 10\n")
@@ -29,12 +34,16 @@ for i in range(size):
 
 # display board(computer board to be added)
     
-def show_pboard(pboard):
-    for row in pboard:
+def show_board(data):
+    """
+    Display the player's or computer's board
+    """
+    for row in data:
         print((" ").join(row))
 
-show_pboard(pboard)
+show_board(pboard)
 
+show_board(cboard)
 
 # create player and computer boats
 # store player + computer boats
